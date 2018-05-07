416326275e50f8a11c793e23a1d987f417df0250
Erik Amundson <SavenTheChickens@gmail.com>
2018-05-07 14:03:04 -0600

    fixed doubleCheck

--- practice.js
+++ practice.js
@@ -44,7 +44,7 @@ function doubleCheck(arr){
       if (arr[i] != "chocolate")
       count++
   }
-  if (count <= arr.length - 1){
+  if (count == arr.length){
     arr.push("chocolate");
   }
   return arr
